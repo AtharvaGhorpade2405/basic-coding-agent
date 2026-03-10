2f50c3c76481645e678cf9b3f838746ef5292d32
AtharvaGhorpade2405 <atharvasg24@gmail.com>
2026-03-10 23:52:43 +0530

    allowing writing to files

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
-from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
+# from functions.get_files_info import get_files_info
+# from functions.get_file_content import get_file_content
+# from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def main():
   working_dir="calculator"
@@ -11,11 +13,15 @@ def main():
   # print(pkg_contents)
   # pkg_contents=get_files_info(working_dir,"../")
   # print(pkg_contents)
-  print(get_file_content(working_dir, "main.py"))
-  print(get_file_content(working_dir, "/bin"))
-  print(get_file_content(working_dir, "pkg/calculator.py"))
-  print(get_file_content(working_dir, "pkg/notexists.py"))
   
+  # print(get_file_content(working_dir, "main.py"))
+  # print(get_file_content(working_dir, "/bin"))
+  # print(get_file_content(working_dir, "pkg/calculator.py"))
+  # print(get_file_content(working_dir, "pkg/notexists.py"))
   
-
+  # print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+  # print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+  # print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+  
+  print(run_python_file("calculator", "main.py"))
 main()
