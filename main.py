05f7a2d9b96d81118a7bcc314c5d758960fbda68
AtharvaGhorpade2405 <atharvasg24@gmail.com>
2026-03-10 16:54:19 +0530

    added verbose flag for dev

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 def main():
@@ -13,15 +14,30 @@ def main():
         print("I need a prompt")
         sys.exit(1)
     
+    verbose_flag=False
+    if len(sys.argv)==3 and sys.argv[2]=="--verbose":
+        verbose_flag=True
+    
     prompt=sys.argv[1]
 
     client = genai.Client(api_key=api_key)
+    
+    messages=[
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
 
     response=client.models.generate_content(
         model="gemini-2.5-flash-lite",
-        contents=prompt
+        contents=messages
     )
 
     print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("Response is malformed.")
+        return
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt Tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response Tokens: {response.usage_metadata.candidates_token_count}")
     
 main()
