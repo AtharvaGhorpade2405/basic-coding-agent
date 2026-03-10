d2bb1eb4f23c6d531de4c39c41e232d4ec5ac543
AtharvaGhorpade2405 <atharvasg24@gmail.com>
2026-03-10 16:40:34 +0530

    getting prompt from command line

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
-load_dotenv()
+def main():
+    
+    load_dotenv()
 
-api_key=os.environ.get("GEMINI_API_KEY")
+    api_key=os.environ.get("GEMINI_API_KEY")
+    
+    if len(sys.argv)<2:
+        print("I need a prompt")
+        sys.exit(1)
+    
+    prompt=sys.argv[1]
 
-client = genai.Client(api_key=api_key)
+    client = genai.Client(api_key=api_key)
 
-response=client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Why is the sky blue?"
-)
+    response=client.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents=prompt
+    )
 
-print(response.text)
+    print(response.text)
+    
+main()
